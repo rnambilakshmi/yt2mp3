8df6f94a0b45d89649da83aaf6680eb65b58f9ac
Nambilakshmi Ramachandran <57563023+rnambilakshmi@users.noreply.github.com>
2020-07-19 14:40:15 +0530

    Add files via upload

---
+++ run.py
@@ -0,0 +1,28 @@
+import youtube_dl
+
+def run():
+    # Ask the user for the video they want to download
+    video_url = input("Please enter the YouTube video url: ")
+    # Download and convert to mp3 and store in downloads folder
+    video_info = youtube_dl.YoutubeDL().extract_info(
+        url=video_url, download=False
+    )
+    filename = f"{video_info['title']}.mp3"
+    options = {
+        'format': 'bestaudio/best',
+        'keepvideo': False,
+        'outtmpl': filename,
+        'postprocessor': [{
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'mp3',
+            'pregerredquality': '256'
+        }]
+    }
+    with youtube_dl.YoutubeDL(options) as ydl:
+        ydl.download([video_info['webpage_url']])
+
+    # Open the file once it has been downloaded
+    subprocess.call(["open", filename])
+
+if __name__ == '__main__':
+    run()
